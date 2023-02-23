39dc3daf086f12b213dc716ff1f479456b6bf177
Adam Johnson <me@adamj.eu>
2023-02-23 09:34:30 +0000

    Update expected exception class for trigger errors

--- pgtrigger/tests/utils.py
+++ pgtrigger/tests/utils.py
@@ -1,14 +1,21 @@
 import contextlib
 
+import django
 from django.db import connections, DEFAULT_DB_ALIAS, transaction as db_transaction
-from django.db.utils import InternalError
+from django.db.utils import InternalError, ProgrammingError
 import pytest
 
 
 @contextlib.contextmanager
 def raises_trigger_error(match=None, database=DEFAULT_DB_ALIAS, transaction=None):
     with contextlib.ExitStack() as stack:
-        stack.enter_context(pytest.raises(InternalError, match=match))
+        # psycopg 3 changes the exception type
+        if django.VERSION >= (4, 2):
+            exc_class = ProgrammingError
+        else:
+            exc_class = InternalError
+
+        stack.enter_context(pytest.raises(exc_class, match=match))
 
         if transaction is None:
             transaction = connections[database].in_atomic_block
